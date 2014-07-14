5cf84b5d6339c19dc671f4e06b34ac09e373b5d9
object1991 <objectes@126.com>
2014-07-14 11:03:16 +0800

    version1.1.8

---
+++ LBase/src/com/leo/base/net/LReqMothed.java
@@ -0,0 +1,37 @@
+package com.leo.base.net;
+
+
+/**
+ * 
+ * @author Chen Lei
+ * @version 1.1.5
+ *
+ */
+public enum LReqMothed {
+	
+	/**
+	 * 使用POST方式请求网络连接
+	 */
+	POST("POST"),
+
+	/**
+	 * 使用GET方式请求网络连接
+	 */
+	GET("GET");
+
+	private String mMothed;
+
+	private LReqMothed(String mothed) {
+		this.mMothed = mothed;
+	}
+
+	/**
+	 * 获取请求网络连接方式具体值
+	 * 
+	 * @return
+	 */
+	public String getMothed() {
+		return this.mMothed;
+	}
+	
+}
